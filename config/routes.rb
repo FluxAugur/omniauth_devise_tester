6fac77a5adac677e2c19eba28a723f35dfa2ca6e
Nathanial L. McConnell <FluxAugur@gmail.com>
2014-06-09 23:08:57 -0400

    updated routes config

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'visitors#index'
   devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
   match '/profile/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], as: :finish_signup
 end
