ecba71774cd52196537873ea3e5424bfde14319e
Nathanial L. McConnell <FluxAugur@gmail.com>
2014-06-09 22:35:46 -0400

    modified development environment config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,8 +13,20 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send.
+  # Email
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: 'localhost:3000'}
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: '587',
+    enable_starttls_auto: true,
+    user_name: Rails.application.secrets.gmail_user,
+    password: Rails.application.secrets.gmail_password,
+    authentication => :plain,
+    domain => 'example.com'
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
